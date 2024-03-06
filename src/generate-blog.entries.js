e1629a3eadc3282ec9b0e91f0de52ead2d2fbdc0
Yc3t <ycetabi@gmail.com>
2024-03-06 13:32:48 +0100

    move to src

--- src/generate-blog.entries.js
+++ src/generate-blog.entries.js
@@ -3,9 +3,9 @@ const path = require('path');
 const MarkdownIt = require('markdown-it');
 const markdownItPrism = require('markdown-it-prism');
 
-const mdDirectory = './public/md/';
-const outputDirectory = './blog-entries/';
-const imageDirectory = '/public/md_images/';
+const mdDirectory = 'public/md/';
+const outputDirectory = 'blog-entries/';
+const imageDirectory = 'public/md_images/';
 
 // Copy Prism.js files to the public directory
 const prismFiles = [
@@ -57,9 +57,9 @@ fs.readdir(mdDirectory, (err, files) => {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Blog Entry</title>
-  <script src="/public/prism.js"></script>
-  <link rel="stylesheet" href="/public/prism-twilight.css">
-  <link rel="stylesheet" href="/public/style.css">
+  <script src="public/prism.js"></script>
+  <link rel="stylesheet" href="public/prism-twilight.css">
+  <link rel="stylesheet" href="public/style.css">
 </head>
 <body>
   <nav id="desktop-nav">
@@ -74,7 +74,7 @@ fs.readdir(mdDirectory, (err, files) => {
       <div class="nav-container">
         <ul class="nav-links">
           <li><a href="/src/index.html">home</a></li>
-          <li><a href="/src/blog.html">blog</a></li>
+          <li><a href="/srcblog.html">blog</a></li>
           <li><a href="/src/contact">contact</a></li>
         </ul>
       </div>
@@ -83,7 +83,7 @@ fs.readdir(mdDirectory, (err, files) => {
   <main>
     <div id="blog-entry">${htmlContent}</div>
   </main>
-  <script src="/public/prism-autoloader.min.js"></script>
+  <script src="public/prism-autoloader.min.js"></script>
 </body>
 </html>
 `;
