13872f1948d04383d240106da809e535c962eb8f
Yc3t <ycetabi@gmail.com>
2024-03-06 13:50:56 +0100

    fixed paths v2

--- generate-blog.entries.js
+++ generate-blog.entries.js
@@ -63,12 +63,14 @@ fs.readdir(mdDirectory, (err, files) => {
 </head>
 <body>
   <nav id="desktop-nav">
-    <div class="logo">
-      <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="11" cy="11" r="11" fill="white"/>
-      </svg>
-      ycet.<br><span class="dev">dev</span>
-    </div>
+  <a href="../index.html"> 
+  <div class="logo">
+    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="11" cy="11" r="11" fill="#D9D9D9"/>
+    </svg>
+    ycet.<br><span class="dev">dev</span>
+  </div>
+  </a>
 
     <div>
       <div class="nav-container">
