2a92810e1b3e539e570fceff48a528fc08159356
Yc3t <ycetabi@gmail.com>
2024-03-07 17:22:18 +0100

    modified blog look

--- generate-blog.entries.js
+++ generate-blog.entries.js
@@ -7,18 +7,6 @@ const mdDirectory = 'public/md/';
 const outputDirectory = 'blog-entries/';
 const imageDirectory = '../public/md_images/';
 
-// Copy Prism.js files to the public directory
-const prismFiles = [
-  'themes/prism-twilight.css',
-  'prism.js',
-  'plugins/autoloader/prism-autoloader.min.js',
-];
-prismFiles.forEach(file => {
-  const srcPath = path.join('node_modules', 'prismjs', file);
-  const destPath = path.join('public', path.basename(file));
-  fs.copyFileSync(srcPath, destPath);
-});
-
 fs.readdir(mdDirectory, (err, files) => {
   if (err) {
     console.error('Error reading the directory:', err);
@@ -46,50 +34,65 @@ fs.readdir(mdDirectory, (err, files) => {
           html: true,
           linkify: true,
           typographer: true,
-        }).use(markdownItPrism);
+        }).use(markdownItPrism, {
+          plugins: ['line-numbers'],
+          init: (prism) => {
+            prism.languages.insertBefore('clike', 'comment', {
+              'doc-comment': {
+                pattern: /(^|[^\\])\/\*\*[\s\S]*?\*\//,
+                lookbehind: true,
+                alias: 'comment'
+              }
+            });
+          },
+        });
 
-        const htmlContent = md.render(updatedMdContent);
+        let htmlContent = md.render(updatedMdContent);
+
+        // Remove the "language-" prefix from the class names
+        htmlContent = htmlContent.replace(/class="language-(\w+)"/g, 'class="$1"');
 
         const blogEntryTemplate = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Blog Entry</title>
-  <script src="../public/prism.js"></script>
-  <link rel="stylesheet" href="../public/prism-twilight.css">
-  <link rel="stylesheet" href="../public/style.css">
-  <link rel="icon" href="public/svg/Pixel Cat.svg" type="image/x-icon">
-</head>
-<body>
-  <nav id="desktop-nav">
-  <a href="../index.html"> 
-  <div class="logo">
-    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="11" cy="11" r="11" fill="#D9D9D9"/>
-    </svg>
-    ycet.<br><span class="dev">dev</span>
-  </div>
-  </a>
-
-    <div>
-      <div class="nav-container">
-        <ul class="nav-links">
-          <li><a href="../index.html">home</a></li>
-          <li><a href="../blog.html">blog</a></li>
-          <li><a href="../contact.html">contact</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <main>
-    <div id="blog-entry">${htmlContent}</div>
-  </main>
-  <script src="../public/prism-autoloader.min.js"></script>
-</body>
-</html>
-`;
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Blog Entry</title>
+            <link rel="stylesheet" href="../public/prism-twilight.css">
+            <link rel="stylesheet" href="../public/style.css">
+            <link rel="icon" href="../public/svg/Pixel Cat.svg" type="image/x-icon">
+          </head>
+          <body>
+            <nav id="desktop-nav">
+              <a href="../index.html">
+                <div class="logo">
+                  <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="11" cy="11" r="11" fill="#D9D9D9"/>
+                  </svg>
+                  ycet.<br><span class="dev">dev</span>
+                </div>
+              </a>
+              <div>
+                <div class="nav-container">
+                  <ul class="nav-links">
+                    <li><a href="../index.html">home</a></li>
+                    <li><a href="../blog.html">blog</a></li>
+                    <li><a href="../contact.html">contact</a></li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+            <main>
+              <div id="blog-entry">${htmlContent}</div>
+            </main>
+            <script src="../public/prism.js"></script>
+            <script>
+              Prism.highlightAll();
+            </script>
+          </body>
+          </html>
+        `;
 
         fs.writeFile(htmlFilePath, blogEntryTemplate, err => {
           if (err) {
